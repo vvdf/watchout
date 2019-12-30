8af3c74c2b7978096b2072854470930bab8ef065
7FFF00 <t42please@gmail.com>
2019-12-29 19:35:43 -0800

    Fix css file linking and commenting basic design

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,7 @@
 // start slingin' some d3 here.
+
+// basic game rules:
+  // movable player piece
+  // randomly generated simple pathing enemies
+  // if player hit, score reset
+  // score increases with time
